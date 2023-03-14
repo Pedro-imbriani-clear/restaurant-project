9f19c9a8eea8b174f34dea11e447b27fffd4736b
Pedro Imbriani <pedro.imbriani.clear@gmail.com>
2023-03-14 12:57:42 -0300

    organizing footer and header

--- saboroso/routes/index.js
+++ saboroso/routes/index.js
@@ -21,21 +21,29 @@ router.get('/', function(req, res, next) {
 router,get('/contacts',function(req,res,next){
   res.render('contacts',{
     title:'contatos - Restaurantes Saboroso!',
+    background:'images/img_bg_3.jpg',
+    h1: 'Diga um oi!'
   });
 });
 router,get('/menus',function(req,res,next){
   res.render('menus',{
     title:'menus - Restaurantes Saboroso!',
+    background:'images/img_bg_1.jpg',
+    h1: 'Saboreie nosso menu!'
   });
 });
 router,get('/reservations',function(req,res,next){
   res.render('reservations',{
     title:'reservas - Restaurantes Saboroso!',
+    background:'images/img_bg_2.jpg',
+    h1: 'Reserve uma Mesa!'
   });
 });
 router,get('/services',function(req,res,next){
   res.render('services',{
     title:'serviços  - Restaurantes Saboroso!',
+    background:'images/img_bg_1.jpg',
+    h1: 'É um prazer poder servir!!'
   });
 });
 module.exports = router;
