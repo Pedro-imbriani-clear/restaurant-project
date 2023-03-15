ea1556a1a75d489c7ac0edf45a8ee922eef2fa89
Pedro Imbriani <pedro.imbriani.clear@gmail.com>
2023-03-15 09:26:17 -0300

    refactoring with includes

--- saboroso/routes/index.js
+++ saboroso/routes/index.js
@@ -1,23 +1,19 @@
 var conn = require('./../inc/db');
 var express = require('express');
+var menus = require('./../inc/menus')
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
-  conn.query(`
-  SELECT * FROM tb_menus ORDER BY title 
-  `,(err,results)=>{
-    if(err){
-      console.log(err);
-    }
-    res.render('index',{
-      title:'Restaurantes Saboroso!',
-      menus:results
-    });
+menus.getMenus().then(results=>{
+  res.render('index',{
+    title:'Restaurantes Saboroso!',
+    menus:results,
+    isHome:true
   });
-  res.render('index', { title: 'Restaurante Saboroso!' });
-});
+  });
+})
+  
 router,get('/contacts',function(req,res,next){
   res.render('contacts',{
     title:'contatos - Restaurantes Saboroso!',
@@ -26,11 +22,14 @@ router,get('/contacts',function(req,res,next){
   });
 });
 router,get('/menus',function(req,res,next){
-  res.render('menus',{
-    title:'menus - Restaurantes Saboroso!',
-    background:'images/img_bg_1.jpg',
-    h1: 'Saboreie nosso menu!'
+  menus.getMenus().then(results=>{
+    res.render('menus',{
+      title:'menus - Restaurantes Saboroso!',
+      background:'images/img_bg_1.jpg',
+      h1: 'Saboreie nosso menu!'
+    });
   });
+ 
 });
 router,get('/reservations',function(req,res,next){
   res.render('reservations',{
